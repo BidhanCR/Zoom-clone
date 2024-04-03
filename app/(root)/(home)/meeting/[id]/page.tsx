07e203285a088c8b1a5af19c1a95cd6f0122bd66
BidhanCR <bidhanroy300805@gmail.com>
2024-04-03 20:21:20 +0600

    creat meeting functionality done

--- app/(root)/(home)/meeting/[id]/page.tsx
+++ app/(root)/(home)/meeting/[id]/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client";
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useParams } from "next/navigation";
+import React, { useState } from "react";
 
-const Meeting = ({params}: {params: {id: string}}) => {
+const Meeting = () => {
+  const { id } = useParams();
+  const { user, isLoaded } = useUser();
+  const [isSetUpComplete, setIsSetUpComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
   return (
-    <div>Meeting Room : #{params.id}</div>
-  )
-}
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetUpComplete ? <MeetingSetup setIsSetUpComplete={setIsSetUpComplete} /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
+};
 
-export default Meeting
+export default Meeting;
