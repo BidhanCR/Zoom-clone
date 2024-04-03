4188d249f4360c612dfda6348fdef183a7b5ca88
BidhanCR <bidhanroy300805@gmail.com>
2024-04-03 12:14:07 +0600

    MeetingModal created

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -18,7 +18,7 @@ const HomeCard = ({
   handleClick,
 }: HomeCardProps) => {
   return (
-    <div className={cn("px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer", className)}>
+    <div className={cn("px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer", className)} onClick={handleClick}>
       <div className="flex-center glassmorphism size-12 rounded-[10px]">
         <Image
           src={img}
